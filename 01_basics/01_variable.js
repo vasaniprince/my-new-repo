3e126b85839e4e69016b9033df22cd3f609887af
Vasani Prince <vasaniprince999@gmail.com>
2025-06-02 13:23:08 +0000

    dataypes and variable

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,17 @@
+const accountId=1546
+let accountEmail="vasaniprince640@gmail.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+let accountState;
+
+// accountId=888
+
+/*
+Prefer not to use var
+bacause of issue in block scope and functional scope
+*/
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountstate])
+
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict";  // treat all JS code as newer version
+
+
+console.log(3+3)
+
+
+let name="Prince"
+let age=21
+let isLoggedIn=false
+
+
+// number => to power 53
+//bigint
+//string => ""
+//null => standalone value
+// undefined => 
+// symbol => unique
+
+
+//object
+
+console.log(typeof name)
+console.log(typeof null)
+console.log(typeof undefined);
+
+
+
