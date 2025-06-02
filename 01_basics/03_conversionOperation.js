41e49cd74606941cdcf122ec4a1429205e4855d5
Vasani Prince <vasaniprince999@gmail.com>
2025-06-02 17:14:35 +0000

    Converation typing

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,34 @@
+// let score=false
+
+// console.log(typeof score);
+
+
+// let valueInNumber=Number(score)
+
+// console.log(valueInNumber);
+
+
+// "33"=> 33
+//"33abc"=> NaN
+
+
+// let isLoggedIn=""
+// console.log(typeof isLoggedIn);
+
+
+// let booleanIdLoggedIn=Boolean(isLoggedIn)
+
+// console.log(typeof booleanIdLoggedIn)
+
+// console.log(booleanIdLoggedIn)
+
+// 1=> true; 0=> false
+//"" => false
+//"prince" => true
+
+let someNumber=33
+
+let stringNumber=String(someNumber)
+
+console.log(typeof stringNumber)
+console.log(stringNumber);
