a812568b68ced31f9ea721f1eae370ebdc9ae954
Vasani Prince <vasaniprince999@gmail.com>
2025-06-03 06:27:19 +0000

    conversionOperation and comparision

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -26,9 +26,54 @@
 //"" => false
 //"prince" => true
 
-let someNumber=33
+// let someNumber=33
 
-let stringNumber=String(someNumber)
+// let stringNumber=String(someNumber)
 
-console.log(typeof stringNumber)
-console.log(stringNumber);
+// console.log(typeof stringNumber)
+// console.log(stringNumber);
+
+
+//********operation**********
+
+
+// let value=3
+
+// let negvalue=-value
+
+// console.log(negvalue);
+
+
+// let str1='hello'
+
+// let str2="prince"
+
+// let str3=str1 + " " + str2
+
+// console.log(str3)
+
+// console.log("1"+5);
+
+// console.log(1 + "2");
+
+
+
+// let num1, num2, num3
+
+// num1=num2=num3=2+2
+
+
+
+let gameCounter=100
+
+gameCounter++;
+
+
+console.log(gameCounter);
+
+// console.log(+true);
+// console.log("");
+
+
+
+
